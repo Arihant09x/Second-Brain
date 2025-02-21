01d1e70393f4816a20e7aaf43b3dded6515d9b0e
Arihant09x <arihantc677@gmail.com>
2025-02-21 14:08:54 +0530

    frontend url fix

--- brainly-frontend/src/components/ShareModal.tsx
+++ brainly-frontend/src/components/ShareModal.tsx
@@ -32,7 +32,8 @@ export function ShareModal({ Shareopen, ShareonClose }: ShareType) {
       if (response.data.link) {
         setShareMessage(response.data.link);
         handleShareClick(
-          "https://second-brain-frontend-ahrl.onrender.com" + response.data.link
+          "https://second-brain-frontend-nwov.onrender.com/" +
+            response.data.link
         ); // Pass the received link
       } else {
         setMessage("Failed to generate link.");
--- brainly-frontend/src/hooks/useContent.tsx
+++ brainly-frontend/src/hooks/useContent.tsx
@@ -1,11 +1,12 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
+import { BACKEND_URL } from "../config";
 
 export function useContent() {
   const [content, setContent] = useState([]);
   function refersh() {
     axios
-      .get("http://localhost:5000/api/v1/content", {
+      .get(BACKEND_URL + "api/v1/content", {
         headers: { Authorization: localStorage.getItem("token") },
       })
       .then((response) => {
